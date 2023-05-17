bbac959a73a81a8a3a641dd797a2ba3e00337aab
LanCaroline <lana.de.sa@hotmail.com>
2023-05-17 10:10:38 -0300

    include images

--- languages-api/src/main/java/com/dls/api/langaguesapi/LanguageController.java
+++ languages-api/src/main/java/com/dls/api/langaguesapi/LanguageController.java
@@ -19,14 +19,74 @@ public class LanguageController {
             new Language(
             "PHP",
             "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
+            2),
+
+            new Language(
+            "C",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/c/c_256x256.png",
+            3),
+
+            new Language(
+            "CSharp",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/csharp/csharp_256x256.png",
+            2),
+
+            new Language(
+            "GO",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/go/go_256x256.png",
+            2),
+
+            new Language(
+            "Haskell",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/haskell/haskell_256x256.png",
+            2),
+
+            new Language(
+            "JavaScript",
+           "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png",
+          2),
+
+            new Language(
+            "Kotlin",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/kotlin/kotlin_256x256.png",
+            2),
+
+            new Language(
+            "Lua",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/lua/lua_256x256.png",
+            2),
+
+            new Language(
+            "Python",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/python/python_256x256.png",
+            2),
+
+            new Language(
+            "R",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/r/r_256x256.png",
+            2),
+
+            new Language(
+            "Ruby",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/ruby/ruby_256x256.png",
+            2),
+
+            new Language(
+            "Swift",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/swift/swift_256x256.png",
+            2),
+
+            new Language(
+            "TypeScript",
+            "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/typescript/typescript_256x256.png",
             2));
 
-    @GetMapping(value = "/favourite-language")
+    @GetMapping("/favourite-language")
     public String processLanguage(){
         return "Come Ranking!";
     }
 
-    @GetMapping(value = "/ranking")
+    @GetMapping("/ranking")
     public List<Language> getLanguages(){return languages;}
 
 }
